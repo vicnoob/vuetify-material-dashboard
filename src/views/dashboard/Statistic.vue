e65d7b61507486f68deda8b056457d1cc9db3046
Vic <thanh.pham@inspectorio.com>
2024-12-09 01:49:25 +0700

    Update 1 more chart

--- src/views/dashboard/Statistic.vue
+++ src/views/dashboard/Statistic.vue
@@ -9,6 +9,23 @@
         cols="12"
         lg="6"
       >
+        <h2>Biểu đồ tình trạng pháp lý</h2>
+        <div class="chart-container">
+          <chartist
+            type="Bar"
+            ref="chartistChart"
+            :data="barChartData"
+            :options="barChartOptions"
+          />  
+        </div>
+        
+      </v-col>
+
+      <v-col
+        cols="12"
+        lg="6"
+      >
+        <h2>Biểu đồ mục đích sử dụng đất</h2>
         <div class="chart-container">
           <chartist
             type="Pie"
@@ -51,6 +68,20 @@
         this.chartData.series.push(data.counts[purpose])
       }
 
+      const complianceCounts = this.landList.reduce((acc, land) => {
+        const status = land.complianceStatus;
+        if (status) {
+          acc[status] = (acc[status] || 0) + 1; // Increment count for each status
+        }
+        return acc;
+      }, {});
+
+      const labels = Object.keys(complianceCounts); // Compliance statuses (categories)
+      const series = Object.values(complianceCounts); // Counts for each compliance status
+      this.barChartData.labels = labels
+      this.barChartData.series = [series]
+      this.barChartOptions.high = Math.max(...series) + 1
+
     },
     data () {
       return {
@@ -67,6 +98,24 @@
             return `${value}\n(${this.chartData.series[index]})`;
           },
         },
+        barChartData: {
+          labels: [],
+          series: [],
+        },
+        barChartOptions: {
+          seriesBarDistance: 10, // Distance between bars
+          axisX: {
+            showGrid: true, // Show grid on x-axis
+          },
+          low: 0,
+          high: 5,
+          axisY: {
+            onlyInteger: true,
+            offset: 20, // Offset on y-axis
+            showLabel: true, // Show labels on y-axis
+          },
+        },
+
       }
     },
 
